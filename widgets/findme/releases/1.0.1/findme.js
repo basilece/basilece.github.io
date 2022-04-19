ad91521f65660df121a280e4cb727acccbc9ef2d
basilece <apostolou.vasilis@hotmail.com>
2022-04-19 17:37:19 +0300

    klk;lk
    Co-Authored-By: basilece <65040193+basilece@users.noreply.github.com>

--- widgets/findme/releases/1.0.1/findme.js
+++ widgets/findme/releases/1.0.1/findme.js
@@ -30,7 +30,21 @@
 //		}	
        
 
-       
+        set TextVal(value) {
+           
+           let request = new XMLHttpRequest();
+	       request.open("GET", "http://apilayer.net/api/validate?access_key=<acess_key>&vat_number=LU26375245");
+	       request.send();
+	       request.onload = () => {
+		         console.log(request);
+		         if (request.status === 200){
+			             console.log(JSON.parse(request.response));
+		             }
+		         else{console.log("something went wrong with API")};
+		         
+	          }
+	          return request.status.toString;
+        }
         
        checkVAT(){
 	       let request = new XMLHttpRequest();
