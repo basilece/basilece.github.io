42b74a54fed36b6743195c72ece1811f455b54ef
basilece <apostolou.vasilis@hotmail.com>
2022-04-20 15:05:37 +0300

    89089098
    Co-Authored-By: basilece <65040193+basilece@users.noreply.github.com>

--- widgets/findme/releases/1.0.1/findme.js
+++ widgets/findme/releases/1.0.1/findme.js
@@ -1,6 +1,7 @@
 (function() {
 	let template = document.createElement("template");
     template.innerHTML = `<link rel="stylesheet" type="text/css" href="https://basilece.github.io/widgets/findme/releases/1.0.1/theme.css"/>`;
+	
 	class Findme extends HTMLElement {
 		constructor() {
 			super();
@@ -10,42 +11,68 @@
 
 			//
 		}
-
 		
-		//Start new lines of code
 		 init() {
             if (this.children.length === 2) return; //constructor called during drag+drop
             if (!this.querySelector("link")) {
                 this.appendChild(template.content.cloneNode(true));
+               }
+		            var ctor = sap.m.Input;
+		          this.DP = new ctor({
+		                change: function () {
+		                    this.fireSubmit();
+		                    this.dispatchEvent(new Event("onSubmit"));
+		                  }.bind(this)  })
+		                  
+               	    this.DP.placeAt(this);
             }
-            var ctor = sap.m.DatePicker;
-            if (this._enablerange) { ctor = sap.m.DateRangeSelection; }
-            this.DP = new ctor({
-                change: function () {
-                    this.fireChanged();
-                    this.dispatchEvent(new Event("onChange"));
-                }.bind(this)
-            }).addStyleClass("datePicker");
-            this.DP.placeAt(this);
-        }
-
-		 fireChanged() {
-            var properties = { dateVal: this.DP.getDateValue() };
-            if (this._enablerange) { properties.secondDateVal = this.DP.getSecondDateValue(); }
-            this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                detail: {
-                    properties: properties
-                }
-            }));
-        }
-
-        set dateVal(value) {
-            if (value == undefined || !this.DP) return;
-            if (typeof (value) === "string") value = new Date(value);
-            this.DP.setDateValue(value);
-        }
+            
+       		 fireSubmit() {
+	            var properties = { TextVal: this.DP.getValue() };            
+	            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+	                detail: {
+	                    properties: properties
+	                }
+	           }));
+	        }    
+           
+            set TextVal(value){
+	             this.DP.setValue(value);
+            }
+		
+		//Start new lines of code
+//		 init() {
+//            if (this.children.length === 2) return; //constructor called during drag+drop
+//            if (!this.querySelector("link")) {
+//                this.appendChild(template.content.cloneNode(true));
+//            }
+//            var ctor = sap.m.DatePicker;
+//            if (this._enablerange) { ctor = sap.m.DateRangeSelection; }
+//            this.DP = new ctor({
+//                change: function () {
+//                    this.fireChanged();
+//                    this.dispatchEvent(new Event("onChange"));
+//                }.bind(this)
+//            }).addStyleClass("datePicker");
+//            this.DP.placeAt(this);
+//        }
+//
+//		 fireChanged() {
+//            var properties = { dateVal: this.DP.getDateValue() };
+//            if (this._enablerange) { properties.secondDateVal = this.DP.getSecondDateValue(); }
+//            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+//                detail: {
+//                    properties: properties
+//                }
+//            }));
+//        }
+//
+//        set dateVal(value) {
+//            if (value == undefined || !this.DP) return;
+//            if (typeof (value) === "string") value = new Date(value);
+//            this.DP.setDateValue(value);
+//        }
         
-
 		//End of lines of code
 
 
