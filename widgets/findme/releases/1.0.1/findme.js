d8c91463850d8576606bd604f60f213f655077b0
basilece <apostolou.vasilis@hotmail.com>
2022-04-20 16:24:58 +0300

    789978879
    Co-Authored-By: basilece <65040193+basilece@users.noreply.github.com>

--- widgets/findme/releases/1.0.1/findme.js
+++ widgets/findme/releases/1.0.1/findme.js
@@ -17,17 +17,26 @@
             if (!this.querySelector("link")) {
                 this.appendChild(template.content.cloneNode(true));
                }
-		            var ctor = sap.m.Input;
-		          this.IT = new ctor({
+//		          var ctor = sap.m.Input;
+//		          this.IT = new ctor({
+//		                change: function () {
+//		                    this.fireSubmit();
+//		                    this.dispatchEvent(new Event("onSubmit"));
+//		                  }.bind(this)  })
+//		                  
+//               	    this.IT.placeAt(this);
+
+                  var ctor = sap.m.Button;
+                		          this.IT = new ctor({
 		                change: function () {
-		                    this.fireSubmit();
-		                    this.dispatchEvent(new Event("onChange"));
+		                    this.firePress();
+		                    this.dispatchEvent(new Event("onSubmit"));
 		                  }.bind(this)  })
 		                  
                	    this.IT.placeAt(this);
             }
             
-       		 fireSubmit() {
+       		 firePress() {
 	            var properties = { TextVal: this.IT.getValue() };            
 	            this.dispatchEvent(new CustomEvent("propertiesChanged", {
 	                detail: {
