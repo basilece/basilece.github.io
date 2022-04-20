1ba1c243b47906b6ef99e56480a30c9d01d3bb1f
basilece <apostolou.vasilis@hotmail.com>
2022-04-20 09:27:08 +0300

    hjjkjk
    Co-Authored-By: basilece <65040193+basilece@users.noreply.github.com>

--- widgets/findme/releases/1.0.1/findme.js
+++ widgets/findme/releases/1.0.1/findme.js
@@ -4,15 +4,15 @@
 		<style>
 		:host {
 			border-radius: 25px;
-			border-width: 4px;
-			border-color: white;
+			border-width: 1px;
+			border-color: gray;
 			border-style: solid;
 			display: block;
 		}
 		</style>
 	`;
 
-	class FindMe extends HTMLElement {
+	class Findme extends HTMLElement {
 		constructor() {
 			super();
 			let shadowRoot = this.attachShadow({mode: "open"});
@@ -23,45 +23,10 @@
 			});
 			this._props = {};
 		}
-		 
-//		onCustomWidgetBeforeUpdate(changedProperties) {
-//			this._props = { ...this._props, ...changedProperties };
-//			console.log(`${this._props["TextVal"]}`);
-//		}	
-       
-//        set TextVal() {
-//           
-//           let request = new XMLHttpRequest();
-//	       request.open("GET", "http://apilayer.net/api/validate?access_key=<acess_key>&vat_number=LU26375245");
-//	       request.send();
-//	       request.onload = () => {
-//		         console.log(request);
-//		         if (request.status === 200){
-//			             console.log(JSON.parse(request.response));
-//		             }
-//		         else{console.log("something went wrong with API")};
-//		         
-//	          }
-//	          return request.status.toString;
-//        }
-        
-//       checkVAT(){
-//	       let request = new XMLHttpRequest();
-//	       request.open("GET", "http://apilayer.net/api/validate?access_key=<acess_key>&vat_number=LU26375245");
-//	       request.send();
-//	       request.onload = () => {
-//		         console.log(request);
-//		         if (request.status === 200){
-//			             console.log(JSON.parse(request.response));
-//		             }
-//		         else{console.log("something went wrong with API")};
-//		         
-//	          }
-//	       
-//           
-//       }
+
+
 
 	}
-	customElements.define("basilece-findme", FindMe);
 
-});
+	customElements.define("basilece-findme", Findme);
+})();
