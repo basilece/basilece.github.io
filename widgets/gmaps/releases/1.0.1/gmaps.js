4e3ec77cae8b7bd49e92adfa05bc068c7036ab7a
basilece <apostolou.vasilis@hotmail.com>
2022-04-22 22:45:03 +0300

    ggkj

--- widgets/gmaps/releases/1.0.1/gmaps.js
+++ widgets/gmaps/releases/1.0.1/gmaps.js
@@ -374,7 +374,7 @@ _.n.Hr=function(){this.Ae()||(this.W||this.H||this.m?zea(this):this.px())};_.n.p
 						 
 						  )
 		             
-               	    //this.BT.placeAt(this);
+               	     //this.BT.placeAt(this);
 					   
                	// Create the MAP 
 				 
