b4dd491684bd582b6857c89e53b281ce0fa66357
basilece <apostolou.vasilis@hotmail.com>
2022-04-22 19:17:43 +0300

    678969876

--- widgets/gmaps/releases/1.0.1/gmaps.js
+++ widgets/gmaps/releases/1.0.1/gmaps.js
@@ -59,7 +59,7 @@
 				   
 				   var geocoder;
 				   var map;
-				   function initialize() {
+				   
 					 geocoder = new google.maps.Geocoder();
 					 var latlng = new google.maps.LatLng(-34.397, 150.644);
 					 var mapOptions = {
@@ -67,7 +67,7 @@
 					   center: latlng
 					 }
 					 map = new google.maps.Map(document.getElementById('map'), mapOptions);
-				   }
+				  
                	    
                	    
                	    
