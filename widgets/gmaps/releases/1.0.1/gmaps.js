de1840fbe43b9bca5f299a7df7a1a511f54fc05a
basilece <apostolou.vasilis@hotmail.com>
2022-04-23 01:07:03 +0300

    89009980

--- widgets/gmaps/releases/1.0.1/gmaps.js
+++ widgets/gmaps/releases/1.0.1/gmaps.js
@@ -325,7 +325,7 @@
 		//
 	let template = document.createElement("template");
     template.innerHTML =`
-		  <div id="divmap"></div>	  
+	<div id="divmap" style="width: 320px; height: 480px;"></div>  
 	 `;
 
 	
