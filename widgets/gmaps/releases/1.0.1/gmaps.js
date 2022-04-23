9873e1982e0589890d9d9c583d84ce6fc801a150
basilece <apostolou.vasilis@hotmail.com>
2022-04-23 10:37:11 +0300

    879789789

--- widgets/gmaps/releases/1.0.1/gmaps.js
+++ widgets/gmaps/releases/1.0.1/gmaps.js
@@ -439,7 +439,7 @@ _.n.Hr=function(){this.Ae()||(this.W||this.H||this.m?zea(this):this.px())};_.n.p
 	                }
 	           }));
 	           
-	           this.getGeocoding(properties.TextVal);
+	           //this.getGeocoding(properties.TextVal);
 	        }    
 	        
 	        
