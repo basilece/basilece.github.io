bc90d2a8785ceff5f6fe34a8f775c1fbc3f266e9
basilece <apostolou.vasilis@hotmail.com>
2022-04-26 18:51:31 +0300

    789789789

--- widgets/gmaps/releases/1.0.2/gmaps.js
+++ widgets/gmaps/releases/1.0.2/gmaps.js
@@ -86,9 +86,10 @@
 							  
 						  })
                           
+						  
 						  var lat = results[0].geometry.location.lat();
                           var lng = results[0].geometry.location.lng();
-                        callback({ Status: "OK", Latitude: lat, Longitude: lng });
+                        callback({ Status: "OK", Latitude: lat, Longitude: lng  });
 						 
 						} else {
 						  alert('Geocode was not successful for the following reason: ' + status);
