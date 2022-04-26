51a47839c84f6c1ccb5f92898ef913c510444bae
basilece <apostolou.vasilis@hotmail.com>
2022-04-26 09:22:46 +0300

    lk;lk;kl;

--- widgets/gmaps/releases/1.0.2/gmaps.js
+++ widgets/gmaps/releases/1.0.2/gmaps.js
@@ -101,7 +101,7 @@
 				
 					firePress() {
 					var address = this.IT.getValue();
-					var coords = new this.generateMap(address);
+					var coords = this.generateMap(address);
 	                console.log(["This is the lat : ", this.generateMap.coords[0]]) ;
 					console.log(["This is the long : ", this.generateMap.coords[1]]) ;
 					var properties = {   TextVal: results[0].formatted_address,
