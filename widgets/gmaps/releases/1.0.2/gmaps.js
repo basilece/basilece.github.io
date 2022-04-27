2a52adaaa9af62a4727b5f250721c804973d3c8a
basilece <apostolou.vasilis@hotmail.com>
2022-04-27 17:00:37 +0300

    890890908

--- widgets/gmaps/releases/1.0.2/gmaps.js
+++ widgets/gmaps/releases/1.0.2/gmaps.js
@@ -109,7 +109,7 @@
 			}
 			var mymap = document.getElementById('divmap');
 			map = new google.maps.Map(document.getElementById('divmap'), mapOptions);
-			//this.append(divmap);
+		     this.append(divmap);
 			geocoder.geocode({ 'address': address }, function (results, status) {
 				if (status == 'OK') {
 					map.setCenter(results[0].geometry.location);
